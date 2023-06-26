6bd4f3cd4b1e6b5c561f887beac2a4930f134257
jobel johny <jobeljohny@gmail.com>
2023-06-26 20:05:24 +0530

    added refersh token

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Login } from 'src/app/enums/config';
 import { LoginSignupComponent } from '../login-signup/login-signup.component';
 import { AuthService } from 'src/app/services/auth.service';
 import { ToasterService } from 'src/app/services/toaster.service';
+import { UserStoreService } from 'src/app/services/user-store.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  username: string = '';
   constructor(
     private dialog: MatDialog,
     private auth: AuthService,
-    private toast: ToasterService
-  ) {
-    //TODO remove
-    setTimeout(() => this.loginSignup(Login.SIGNUP), 100);
+    private toast: ToasterService,
+    private userStore: UserStoreService
+  ) {}
+
+  ngOnInit(): void {
+    this.userStore.getUserName().subscribe((val) => {
+      let uname = this.auth.getUsername();
+      this.username = val || uname;
+    });
   }
 
   get Login() {
