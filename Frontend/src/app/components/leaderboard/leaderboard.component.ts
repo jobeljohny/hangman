ed989e4941de660a1b718300396b6a6c19ce4b85
jobel johny <jobeljohny@gmail.com>
2023-06-30 15:22:24 +0530

    leaderboard styling phase 2

--- Frontend/src/app/components/leaderboard/leaderboard.component.ts
+++ Frontend/src/app/components/leaderboard/leaderboard.component.ts
@@ -10,12 +10,27 @@ import { ApiService } from 'src/app/services/api.service';
 export class LeaderboardComponent implements OnInit {
   leaderboards: UserStatistic[] = [];
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) {
+    //TODO remove this
+    this.generateLB();
+  }
 
   ngOnInit() {
     this.api.getLeaderboard().subscribe((data) => {
       this.leaderboards = data;
       console.log(this.leaderboards);
     });
+  }
+
+  generateLB() {
+    const count = 50;
+    for (let index = 1; index <= count; index++) {
+      let stat = new UserStatistic();
+      stat.username = (Math.random() + 1).toString(36).substring(5);
+      stat.highestRound = Math.floor(Math.random() * (16 - 4 + 1)) + 4;
+      stat.highscore =
+        Math.round((Math.random() * (300 - 100 + 1) + 100) * 100) / 100;
+      this.leaderboards.push(stat);
+    }
   }
 }
