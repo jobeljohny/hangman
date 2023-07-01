5df7833a327f0bd61ab7339689ad91bcb62eacac
godwinvs <godwinkurapika@gmail.com>
2023-07-01 17:22:19 +0530

    stats-page structure updates

--- Frontend/src/app/components/stats-page/stats-page.component.ts
+++ Frontend/src/app/components/stats-page/stats-page.component.ts
@@ -9,12 +9,9 @@ import { ApiService } from 'src/app/services/api.service';
 export class StatsPageComponent implements OnInit {
   users: any = [];
 
-  username = 'brute7';
+  username = 'Brute7';
   firstname = 'Godwin';
   lastname = 'VS';
-  gamesPlayed = 45;
-  highestScore = 456.56;
-  highestRound = 8;
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
